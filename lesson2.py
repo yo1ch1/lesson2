9319e2a1528117ba88dfb6c3cc55a97b44d683ed
yo1ch1 <dmit.petukhov2017@gmail.com>
2024-08-20 20:31:31 +1200

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,5 @@
+total_task=12
+hour_spent=1.5
+course_name='Python'
+average_execution_time=hour_spent/total_task
+print('Курс:',course_name,', ''всего задач:',total_task,', ''затрачено часов:',hour_spent,', ''среднее время выполнения',average_execution_time)
